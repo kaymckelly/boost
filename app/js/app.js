02e89d501fe91e6986bc7b43dcb5b1a6f0f0d286
Kay McKelly <frecklestars@gmail.com>
2016-06-29 15:06:34 -0700

    Working on routes

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,24 @@
-var app = angular.module('boostApp', ['ui.bootstrap', 'ngAnimate', 'ngTouch']);
+(function() {
+  'use strict';
+
+  angular
+    .module('boostApp', [
+      'ui.bootstrap',
+      'ngAnimate',
+      'ngTouch',
+      'ngRoute'
+    ])
+    .config(config);
+
+    function config($routeProvider, $locationProvider) {
+      $routeProvider
+        .when('/timer', {
+          templateUrl: '/timer.html',
+          controller: 'timerCtrl'
+        })
+        .otherwise({
+          redirectTo: '/'
+        })
+    $locationProvider.html5Mode(true);
+  };
+})();
