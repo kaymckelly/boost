edf176908d5ec5bcc2aeb80e36c8569ef7cd859c
Kay McKelly <frecklestars@gmail.com>
2016-06-30 14:38:36 -0700

    Styles more or less working and routing issues resolved

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,8 +5,12 @@
     .module('boostApp')
     .controller('NavController', NavController);
 
-    function NavController() {
+    function NavController($location) {
       var vm = this;
       vm.isCollapsed = false;
-    }
+      vm.menuClass = function(page) {
+        var current = $location.path().substring(1);
+        return page === current ? "active" : "";
+      };
+    };
 })();
