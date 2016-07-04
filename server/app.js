ab0407e9b2645921c569638c981d52d67697aa02
Kay McKelly <frecklestars@gmail.com>
2016-07-04 09:07:46 -0700

    Express server running, still figuring out details, testing databases

--- server/app.js
+++ server/app.js
@@ -14,8 +14,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -31,6 +29,7 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
+
 
 // error handlers
 
