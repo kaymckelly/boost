59e0758b759d36903f4cae0f29d7f86ee297c771
Kay McKelly <frecklestars@gmail.com>
2016-07-14 13:03:21 -0700

    Switching to focus on Auth0 to implement oAuth

--- server/app.js
+++ server/app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var cors = require('cors');
+var passport = require('passport');
+var InstagramStrategy = require('passport-instagram').Strategy;
 
 var api = require('./api');
 
@@ -12,17 +14,48 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-// TODO: cors will need to be recoded upon deployment, as origin of req will change
-app.use(cors());
 
+// TODO: cors will need to be recoded upon deployment, as origin of req will change. make sure req is from a single specified origin, for security's sake
+app.use(cors());
+app.use(passport.initialize());
 app.use('/api', api);
+
+// TODO: serialize users if this doesn't work??
+
+passport.use(new InstagramStrategy({
+  insta_id: INSTAGRAM_KEY,
+  clientSecret: INSTAGRAM_SECRET,
+  callbackURL: 'http:127.0.0.1:3000/auth/instagram/callback'
+  },
+  function(accessToken, refreshToken, profile, done) {
+    // mongoose but knex call instead: users where insta_id = profile.id
+    // select 1st from users db to match this instagramId. if they don't exist, create them. if they do, send no error and the user info.
+    knex('users').first().where(insta_id == users.insta_id).then(function(user) {
+      if(!user) {
+        knex('users').insert({
+          // TODO: will I need to insert at index 0? does it matter?
+          insta_id: insta_id
+        }, '*').then(function(user) {
+          done(null, user[0]);
+        });
+        // TODO: redirect to profile edit page
+      }
+      else if(user) {
+        // TODO: redirect to profile edit page iff bio and username exist (if not, redirect to profile edit page)
+        done(null, user[0]);
+      }
+      else {
+        done(error);
+      }
+    })
+}));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
